1c9bcd5348f6a696240552e43e729783d3d29900
sam-wheat <sam.wheat@outlook.com>
2017-01-28 06:01:30 -0800

    Add AggressiveMode features

--- LeaderAnalytics.AdaptiveClient/src/LeaderAnalytics.AdaptiveClient/ClientFactory.cs
+++ LeaderAnalytics.AdaptiveClient/src/LeaderAnalytics.AdaptiveClient/ClientFactory.cs
@@ -66,5 +66,69 @@ namespace LeaderAnalytics.AdaptiveClient
             }
             return client;
         }
+
+
+        private T CreateClient(Func<T> evaluator, params string[] overrideNames)
+        {
+            endPointContext.CurrentEndPoint = CurrentEndPoint = null;
+            T client = default(T);
+            IPerimeter perimeter = epcFactory(typeof(T));
+            IEnumerable<IEndPointConfiguration> endPoints;
+
+            if (perimeter == null)
+                return client;
+
+            if (overrideNames != null && overrideNames.Any())
+                endPoints = perimeter.EndPoints.Where(x => overrideNames.Any(o => x.Name == o));
+            else
+                endPoints = perimeter.EndPoints;
+
+            foreach (IEndPointConfiguration endPoint in endPoints)
+            {
+                // must set CurrentEndPoint before calling serviceFactory
+                endPointContext.CurrentEndPoint = CurrentEndPoint = endPoint;
+                client = serviceFactory(endPoint.EndPointType);
+
+                if (client == null)
+                    continue;
+
+                bool isClientFound = true;
+
+                try
+                {
+                    evaluator();
+                }
+                catch (Exception ex)
+                {
+                    isClientFound = false;
+                }
+                
+
+                if (!isClientFound)
+                    continue;
+                
+                break;
+            }
+            return client;
+        }
+
+        private bool CallClient<TResult>(Func<T, TResult> client, IEndPointConfiguration ep)
+        {
+            bool result = true;
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            return result; 
+        }
+
+        private bool CallValidator(IEndPointValidator validator, IEndPointConfiguration ep)
+        {
+            return validator.IsInterfaceAlive(ep);
+        }
     }
 }
