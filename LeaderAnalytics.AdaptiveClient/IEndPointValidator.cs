9f016f03641f91d12d7cf66c84a5e67a6181d8e1
Sam <sam.wheat@outlook.com>
2021-11-27 11:59:09 -0800

    Add IsInterfaceAliveAsync

--- LeaderAnalytics.AdaptiveClient/IEndPointValidator.cs
+++ LeaderAnalytics.AdaptiveClient/IEndPointValidator.cs
@@ -1,9 +1,10 @@
-﻿
+﻿using System.Threading.Tasks;
 
 namespace LeaderAnalytics.AdaptiveClient
 {
     public interface IEndPointValidator
     {
         bool IsInterfaceAlive(IEndPointConfiguration endPoint);
+        Task<bool> IsInterfaceAliveAsync(IEndPointConfiguration endPoint);
     }
 }
